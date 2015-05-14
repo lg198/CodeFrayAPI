8570181fcb5adb76d060210a2f6a43d0728294bd
Layne Gustafson <lg198@pvsd.org>
2015-05-14 08:51:06 -0400

    Added getRound to Game.

--- src/com/github/lg198/codefray/api/game/Game.java
+++ src/com/github/lg198/codefray/api/game/Game.java
@@ -7,4 +7,5 @@ public interface Game {
     public boolean isHoldingFlag(Team t);
     public Point getFlagLocation(Team t);
     public Direction getFlagDirection(Team t);
+    public long getRound();
 }

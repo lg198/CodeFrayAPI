b5c4a79869dc1f4bb6cae4a9667b0754a36d529d
Layne Gustafson <lg198@pvsd.org>
2015-09-07 03:23:18 +0100

    Golems now only recognize cardinal directions.

--- src/com/github/lg198/codefray/api/math/Direction.java
+++ src/com/github/lg198/codefray/api/math/Direction.java
@@ -2,8 +2,8 @@ package com.github.lg198.codefray.api.math;
 
 public enum Direction {
 
-    NORTH(0, -1), NORTH_EAST(1, -1), EAST(1, 0), SOUTH_EAST(1, 1),
-    SOUTH(0, 1),  SOUTH_WEST(-1, 1), WEST(-1, 0), NORTH_WEST(-1, -1);
+    NORTH(0, -1),  EAST(1, 0),
+    SOUTH(0, 1),  WEST(-1, 0);
 
     Direction(int x, int y) {
         xc = x;
